2a3e80c05d97eacdcbcb95b49e69fd62debf688b
Vicky Maulana <muhammad.vicky@ui.ac.id>
2023-02-28 14:24:54 +0700

    cleaning

--- lib/main.dart
+++ lib/main.dart
@@ -56,26 +56,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      // add the navbar
       bottomNavigationBar: const Navbar(),
-      // bottomNavigationBar: BottomNavigationBar(
-      //   items: const <BottomNavigationBarItem>[
-      //     BottomNavigationBarItem(
-      //       icon: Icon(Icons.home),
-      //       label: 'Home',
-      //     ),
-      //     BottomNavigationBarItem(
-      //       icon: Icon(Icons.search),
-      //       label: 'Search',
-      //     ),
-      //     BottomNavigationBarItem(
-      //       icon: Icon(Icons.person),
-      //       label: 'Profile',
-      //     ),
-      //   ],
-      //   currentIndex: 0,
-      //   selectedItemColor: Colors.amber[800],
-      // ),
     );
   }
 }
