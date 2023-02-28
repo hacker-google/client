1ae1d103214f1c22f87bea695be5a466d6a5ed6a
Vicky Maulana <muhammad.vicky@ui.ac.id>
2023-02-28 14:23:54 +0700

    initialize navbar

---
+++ lib/navbar.dart
@@ -0,0 +1,31 @@
+// create a code snippet for the navbar
+
+// Path: lib/navbar.dart
+
+import 'package:flutter/material.dart';
+
+class Navbar extends StatelessWidget {
+  const Navbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.search),
+          label: 'Search',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: 'Profile',
+        ),
+      ],
+      currentIndex: 0,
+      selectedItemColor: Colors.amber[800],
+    );
+  }
+}
