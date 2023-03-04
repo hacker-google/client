3434814642cd15fa5dddd5c5f3e563a5a6a0d815
iqbalpa <iqbalpahlevi16@gmail.com>
2023-03-04 16:53:38 +0700

    implement daftar pengaduan page #1

--- lib/views/pages/report/_report.dart
+++ lib/views/pages/report/_report.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import '../../widgets/navbar/_navbar.dart';
 
--- lib/views/pages/report/daftar_pengaduan.dart
+++ lib/views/pages/report/daftar_pengaduan.dart
@@ -8,8 +8,75 @@ class ReportPage extends StatefulWidget {
 }
 
 class _ReportPageState extends State<ReportPage> {
+  final List<Map<String, dynamic>> _reports = [
+    {
+      'id': 1,
+      'title': 'Perampokan',
+      'proses': 'Dalam Proses',
+      'tanggal': '12 Januari 2021',
+    },
+    {
+      'id': 2,
+      'title': 'Pencurian',
+      'proses': 'selesai',
+      'tanggal': '30 Februari 2021',
+    },
+    {
+      'id': 3,
+      'title': 'Perampokan dan pencurian',
+      'proses': 'Ditolak',
+      'tanggal': '11 Januari 2021',
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Crime Space'),
+      ),
+      body: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text(
+              'Daftar Pengaduan',
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          const ListTile(
+            leading: Icon(Icons.sort),
+            title: Text('Sort by Status'),
+          ),
+          SizedBox(
+            // get the maximum of height of the screen
+            height: MediaQuery.of(context).size.height - 500,
+            child: ListView.builder(
+              itemCount: _reports.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: Text(_reports[index]['title']),
+                  subtitle: Text(_reports[index]['proses']),
+                  trailing: Text(_reports[index]['tanggal']),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushNamed(context, '/tambah_pengaduan');
+        },
+        child: const Row(children: [
+          Icon(Icons.add),
+          // Text('report'),
+        ]),
+      ),
+      bottomNavigationBar: const Navbar(),
+    );
   }
 }
